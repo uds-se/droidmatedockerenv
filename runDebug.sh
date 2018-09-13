b40066b3c240ef662925ea5a98f4407428b6d060
Timo Gühring <timo.guehring@googlemail.com>
2018-09-13 14:57:06 +0200

    adb devices command works in interactive session

--- runDebug.sh
+++ runDebug.sh
@@ -1,11 +1,15 @@
 # Run this for debugging purposes. This creates an interactive shell
 docker run \
+    --net=host \
     -it \
-    -e APK_FOLDER_SRC='/Users/timtoheus/Projects/droidmatenew/droidmate/apks2/originals/' \
-    -v /Users/timtoheus/Library/Android/sdk/platform-tools/adb:/usr/local/bin/adb \
-    testingenv:latest \
+    -v /opt/Android/Sdk/platform-tools/adb:/usr/local/bin/adb \
+    farmtesting:latest \
     /bin/bash
 
+
+    # --privileged \
+    # -e APK_FOLDER_SRC='/Users/timtoheus/Projects/droidmatenew/droidmate/apks2/originals/' \
+    # -v /opt/Android/Sdk/platform-tools/adb:/usr/local/bin/adb \
     # --mount type=bind,source="/Users/timtoheus/Library/Android/sdk/platform-tools/adb",target=/usr/local/bin/adb \
 
 # docker cp <containerId>:/file/path/within/container /host/path/target
