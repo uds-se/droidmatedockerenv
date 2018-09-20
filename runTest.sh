87c352e7134123d9b425aa3f39233ae546efcffe
Timo Gühring <timo.guehring@googlemail.com>
2018-09-20 14:56:23 +0200

    Introduce additional parameter parsing for dependencies

--- runTest.sh
+++ runTest.sh
@@ -20,7 +20,42 @@ touch args.txt
 if [[ "$#" -ge 1 ]]; then
 	echo -n "--Exploration-deviceSerialNumber=$1" > args.txt
 fi
-for i in ${@:2} ; do
+
+if [[ $2 != "[" ]]; then
+    echo "Expected [ as next character, but was $2"
+    exit 1
+fi
+
+# Process dependencies
+echo "Process dependencies"
+INDEX=3
+SubStr="="
+for i in ${@:${INDEX}}; do
+    echo "Param $i"
+    INDEX=$((INDEX+1))
+    if [[ "${i}" == "]" ]]; then
+        break
+    fi
+    keyword=${i%%=*}
+    val=${i#*=}
+    case "$keyword" in
+        ("dependency")
+            echo "We have a dependency"
+            mkdir ${INDEX}
+            git clone ${val} ${INDEX}
+            cd ${INDEX}
+            ./gradlew install
+            ;;
+        (*)
+            echo "Not supported action: $keyword"
+            ;;
+    esac
+done
+
+# Process DroidMate parameters
+echo "Process DroidMate parameters"
+for i in ${@:${INDEX}}; do
+    echo " $i"
 	echo -n " $i" >> args.txt
 done
 echo -n " --Exploration-apksDir=${APK_FOLDER_CONTAINER} --Output-outputDir=${TOOL_OUTPUT_FOLDER}" >> args.txt
